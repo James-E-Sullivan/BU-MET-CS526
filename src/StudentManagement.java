6a4e46be28a58612f4c8de0fe3e5e4387e808e8a
james.sullivan <jesullivan222@gmail.com>
2019-03-25 22:34:25 -0400

    Added first option to StudentManagement.java.

--- src/StudentManagement.java
+++ src/StudentManagement.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.LinkedList;
 
 public class StudentManagement {
 
@@ -6,6 +7,9 @@ public class StudentManagement {
 
         boolean done = false;
         Scanner input = new Scanner(System.in);
+
+        /* Linked List Declaration */
+        LinkedList<Student> studentList = new LinkedList<Student>();
 
         // loop continues until user chooses to exit
         while ( !done ){
@@ -25,8 +29,14 @@ public class StudentManagement {
             }else{
                 System.out.println("Number not entered, try again.");
                 input.next();  // bad input must be discarded
-                continue
+                continue;
             }
+
+            // For testing purposes. To be deleted.
+            System.out.println(studentList.size());
+
+
+
 
             // check valid selection
             if (selection < 1 || selection > 6){
@@ -36,8 +46,37 @@ public class StudentManagement {
 
             if (selection == 1){
                 //add code here
+                System.out.println("(Add Student) Please enter a student ID: ");
+                String newID = input.next();
+
+                // tests for duplicate studentID values in the linked list
+                boolean duplicate = false;
+                for (int j=0; j<studentList.size(); j++){
+                    Student currentStudent = studentList.get(j);
+                    if (newID.equals(currentStudent.getID())){
+                        duplicate = true;
+                        System.out.println("A student with ID " + newID + " is already in the list.");
+                        break;
+                    }
+                }
+
+                if (!duplicate){
+                    System.out.println("Please enter the student's name: ");
+                    String newName = input.next();
+                    System.out.println("Please enter the student's GPA: ");
+                    double newGPA = input.nextDouble();
+
+                    // creates a student object and adds it to studentList
+                    Student newStudent = new Student(newID, newName, newGPA);
+                    studentList.add(newStudent);
+                    System.out.println("Student successfully added to list.");
+                }
+
+                continue;
+
             }
 
+            /*
             if (selection == 2){
                 //add code here
             }
@@ -57,7 +96,9 @@ public class StudentManagement {
             if (selection == 6){  // exit
                 System.out.println("Goodbye.");
                 done = true;
+
             }
+            */
         }
     }
 }

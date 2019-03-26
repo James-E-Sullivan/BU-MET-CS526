8a718360172dc95d5c2e985ae2d00fa39fd6359d
james.sullivan <jesullivan222@gmail.com>
2019-03-26 01:12:35 -0400

    Completed and commented StudentManagement.java

--- src/StudentManagement.java
+++ src/StudentManagement.java
@@ -3,17 +3,38 @@ import java.util.LinkedList;
 
 public class StudentManagement {
 
+    /** Method used to determine whether a student ID is in a Linked List of Student objects
+     * Returns index of matching Student object if there is a match
+     * Returns -1 if there is no match*/
+    private static int studentInList(String id, LinkedList<Student> list){
+        int index = 0;
+        for (Student s : list){     // iterates through Student elements in LinkedList
+            if (id.equals(s.getID())){
+                return index;    // returns index of student if there is a matching ID
+            }
+            index++;
+        }
+        return -1;           // returns -1 to indicate that there was no matching ID
+    }
+
+    /** Prints the name, ID, and GPA of a student on 3 lines. */
+    private static void displayStudentInfo(Student s){
+        System.out.println("\nName: " + s.getName());
+        System.out.println("Student ID: " + s.getID());
+        System.out.println("GPA: " + s.getGPA());
+    }
+
     public static void main(String[] args) {
 
         boolean done = false;
         Scanner input = new Scanner(System.in);
 
-        /* Linked List Declaration */
-        LinkedList<Student> studentList = new LinkedList<Student>();
+        /* Linked List Declaration (elements are Student objects) */
+        LinkedList<Student> studentList = new LinkedList<>();
 
         // loop continues until user chooses to exit
         while ( !done ){
-            System.out.println("Choose an option: \n");
+            System.out.println("\nChoose an option: \n");
             System.out.println("1. Add a student");
             System.out.println("2. Remove a student");
             System.out.println("3. Update student GPA");
@@ -32,12 +53,6 @@ public class StudentManagement {
                 continue;
             }
 
-            // For testing purposes. To be deleted.
-            System.out.println(studentList.size());
-
-
-
-
             // check valid selection
             if (selection < 1 || selection > 6){
                 System.out.println("Please select a number between 1 and 6, inclusive.");
@@ -45,60 +60,102 @@ public class StudentManagement {
             }
 
             if (selection == 1){
-                //add code here
                 System.out.println("(Add Student) Please enter a student ID: ");
                 String newID = input.next();
 
-                // tests for duplicate studentID values in the linked list
-                boolean duplicate = false;
-                for (int j=0; j<studentList.size(); j++){
-                    Student currentStudent = studentList.get(j);
-                    if (newID.equals(currentStudent.getID())){
-                        duplicate = true;
-                        System.out.println("A student with ID " + newID + " is already in the list.");
-                        break;
-                    }
-                }
+                // gets LinkedList index of Student w/ matching ID (or -1 if no matching ID)
+                int studentIndex = studentInList(newID, studentList);
 
-                if (!duplicate){
+                if (studentIndex == -1){    // if there is no matching student ID in the list
+                    // prompts new student's name and GPA
                     System.out.println("Please enter the student's name: ");
                     String newName = input.next();
                     System.out.println("Please enter the student's GPA: ");
                     double newGPA = input.nextDouble();
 
-                    // creates a student object and adds it to studentList
+                    // creates a student object with input ID, Name, and GPA; adds it to studentList
                     Student newStudent = new Student(newID, newName, newGPA);
                     studentList.add(newStudent);
                     System.out.println("Student successfully added to list.");
+                }else{      // if there is a matching student ID in the list
+                    System.out.println("A student with ID " + newID + " is already in the list.");
                 }
-
-                continue;
+                continue;       // returns to main menu
 
             }
 
-            /*
+
             if (selection == 2){
-                //add code here
+                System.out.println("(Remove Student) Please enter a student ID: ");
+                String inputID = input.next();
+
+                // gets LinkedList index of Student w/ matching ID (or -1 if no matching ID)
+                int studentIndex = studentInList(inputID, studentList);
+
+                if (studentIndex == -1){        // if there is no matching student ID in the list
+                    System.out.println("Student identified by ID " + inputID + " is not in the list.");
+                }else{      // if there is a matching ID in the list
+                    studentList.remove(studentIndex);       // removes student at index with matching ID
+                    System.out.println("Student successfully removed from list.");
+                }
+                continue;       // returns to main menu
             }
+
 
             if (selection == 3){
-                //add code here
+                System.out.println("(Update GPA) Please enter a student ID: ");
+                String inputID = input.next();
+
+                boolean duplicate = false;
+                for (Student s : studentList){              // loops through Students in studentList
+                    if (inputID.equals(s.getID())){         // if user input ID is equal to the Student ID
+                        duplicate = true;
+                        System.out.println("Please enter the student's new GPA: ");
+                        s.setGPA(input.nextDouble());       // GPA of student s set to double input by user
+                        System.out.println("Successfully updated GPA.");
+                        break;      // exit for loop after matching ID is found
+                    }
+                }
+
+                if (!duplicate){        // if no matching ID was found
+                    System.out.println("Student identified by ID " + inputID + " is not in the list.");
+                }
+                continue;       // returns to main menu
             }
+
 
             if (selection == 4){
-                //add code here
+                System.out.println("(Display Student Info) Please enter a student ID: ");
+                String inputID = input.next();
+
+                boolean duplicate = false;
+                for (Student s : studentList){              // loops through Students in studentList
+                    if (inputID.equals(s.getID())){
+                        duplicate = true;
+                        displayStudentInfo(s);              // displays name, id, and GPA of matching student
+                        break;                              // exit for loop
+                    }
+                }
+                if (!duplicate){        // if no matching ID was found
+                    System.out.println("Student identified by ID " + inputID + " is not in the list.");
+                }
+                continue;       // returns to main menu
             }
+
 
             if (selection == 5){
-                //add code here
+                for (Student s : studentList){              // loops through Students in studentList
+                    displayStudentInfo(s);                  // displays name, id, and GPA of each student
+                }
             }
 
+
             if (selection == 6){  // exit
-                System.out.println("Goodbye.");
+                System.out.println("Bye.");
                 done = true;
 
             }
-            */
         }
+        input.close();
     }
 }
